3cbc1e1d594501fda316a10c1d2727754c4bac9f
cjchamp4now <alan.coryjackson@gmail.com>
2019-04-26 17:31:55 -0500

    Fixed missing files(by making app not need them), added html elements for each possible JSON item to display. TODO(still): error handling, load api based off page url.

--- src/app/rip-json.service.ts
+++ src/app/rip-json.service.ts
@@ -2,27 +2,21 @@ import { Injectable } from "@angular/core";
 import { Observable, throwError } from "rxjs";
 import {HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError, map } from "rxjs/operators";
-import { comicData } from './comicData'
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class RipJSONService {
-
-
-  urlbase: string = "https://xkcd.com/";
-  urlend: string = "info.0.json";
-  urlmid: string = "";
+ //to add error handling
+  urlbase: string = "https://xkcd.now.sh/";
+  urlend: string = "";
   whichAPI: boolean;
     
   constructor(private http: HttpClient ) { }
 
-  getData(): Observable<comicData[]>{
-        //return //this.http.get(this.urlbase + urlmid + this.urlend).map(res => res.json()); //TODO finish url swap
-        //return this.http.get<comicData[]>(this.urlbase + this.urlmid + this.urlend, httpOptions); //array?
-        //https://xkcd.now.sh/
-        return this.http.get<comicData[]>("https://xkcd.now.sh/1000"); //https://github.com/mrmartineau/xkcd-api
+  
+  getData(){
+        return this.http.get(this.urlbase + this.urlend); //https://github.com/mrmartineau/xkcd-api
   }
   
   geturlType(){ //to add ability to go with eith recent api or specific comic api once urlscheme and routing is more set in stone
--- src/app/rip-json/rip-json.component.ts
+++ src/app/rip-json/rip-json.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { xkcdData } from '../xkcdData';
 import { RipJSONService } from '../rip-json.service';
 
 @Component({
@@ -8,41 +7,11 @@ import { RipJSONService } from '../rip-json.service';
   styleUrls: ['./rip-json.component.css']
 })
 export class RipJSONComponent implements OnInit {
-  xkcddata: xkcdData;
   public rawData = [];
   constructor(private apiService: RipJSONService) {}
 
-  testdata: xkcdData = {
-    month: "2",
-    comicnum: 9999,
-    link: "url",
-    year: "1972",
-    news: "",
-    safe_title: "string",
-    transcript: "string",
-    altText: "hello, this is altText",
-    comicIMG: "comicURLHERE",
-    title: "testTIME",
-    day: "32"
-  };
-
-
-
   ngOnInit() {
-    //this.apiService
-      //.getData//TODO
-
-    //this.getData();
     this.apiService.getData()
       .subscribe(data => this.rawData = data);
   }
-
-  getData(): void{
-    //this.apiService.getData()
-      //.subscribe((data => this.data = {
-
-      //});
-    
-  }
-
 }
