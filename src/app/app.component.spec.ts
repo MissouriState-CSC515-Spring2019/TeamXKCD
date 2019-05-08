01dd1cc835cb6e93a27f40686a934b2d7803709e
Lauren Tallerico <lat13@live.missouristate.edu>
2019-05-08 16:10:41 -0500

    arrow hover more obvious, but not pink, lol

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,18 +14,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-
-  // it(`should have as title 'xkcd-project515'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('xkcd-project515');
-  // });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
